ecf377709e8d88f62c15dd5ff14776d48a5613b9
Gustavo <tavitooo14@icloud.com>
2024-09-15 00:02:36 -0300

    hola

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     
@@ -19,7 +19,5 @@ root.render(
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
--- src/services/FireStore.js
+++ src/services/FireStore.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, addDoc, query, where, collection, getDocs, Timestamp} from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAg0aPVir-UJUBq5gOJCQh27xKApQYW1U8",
     authDomain: "itech-store-b28a3.firebaseapp.com",
